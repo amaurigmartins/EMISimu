93aec6c0943b7e421ac43556ff5c029c68887c9a
amaurigmartins <amaurigmartins@gmail.com>
2021-03-03 16:00:01 -0300

    Added main.m

---
+++ main.m
@@ -0,0 +1,21 @@
+% initialize the matlab workspace
+clear; 
+close all; 
+clc;
+
+% enter the project directory name here
+project_name = 'SimpleInterferenceCase';
+
+addpath(project_name);
+addpath('functions');
+
+% define the problem
+define_source_line_data
+define_target_line_data
+define_resistivity
+
+% build LCC cards
+
+% build main ATP card
+
+% run stuff

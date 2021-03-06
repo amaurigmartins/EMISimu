4313dd5e01f20420362473d20803e147dd806f5c
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-06 16:48:41 -0300

    changed variables in getTheveninParams

--- functions/getTheveninParams.m
+++ functions/getTheveninParams.m
@@ -1,5 +1,6 @@
 function [term1, term2] = getTheveninParams(app)
-ncirc=valIfTrue(app,app.DoublecircuitButton.Value==1,2,1);
+% ncirc=valIfTrue(app,app.DoublecircuitButton.Value==1,2,1);
+ncirc = app.NumCircuits.Value;
 
 term1(1,1)=app.R0S1EditFieldT1.Value; %R0
 term1(1,2)=app.L0S1EditFieldT1.Value; %L0
@@ -12,15 +13,15 @@ term2(1,3)=app.R1S1EditFieldT2.Value; %R+
 term2(1,4)=app.L1S1EditFieldT2.Value; %L+
 
 if ncirc==2
-    term1(2,1)=app.R0S2EditFieldT1.Value; %R0
-    term1(2,2)=app.L0S2EditFieldT1.Value; %L0
-    term1(2,3)=app.R1S2EditFieldT1.Value; %R+
-    term1(2,4)=app.L1S2EditFieldT1.Value; %L+
+    term1(2,1)=app.R0S2T1.Value; %R0
+    term1(2,2)=app.L0S2T1.Value; %L0
+    term1(2,3)=app.R1S2T1.Value; %R+
+    term1(2,4)=app.L1S2T1.Value; %L+
     
-    term2(2,1)=app.R0S2EditFieldT2.Value; %R0
-    term2(2,2)=app.L0S2EditFieldT2.Value; %L0
-    term2(2,3)=app.R1S2EditFieldT2.Value; %R+
-    term2(2,4)=app.L1S2EditFieldT2.Value; %L+
+    term2(2,1)=app.R0S2T2.Value; %R0
+    term2(2,2)=app.L0S2T2.Value; %L0
+    term2(2,3)=app.R1S2T2.Value; %R+
+    term2(2,4)=app.L1S2T2.Value; %L+
 
 end
 

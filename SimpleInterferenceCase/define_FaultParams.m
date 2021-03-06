86de00f1d02a123b0e38065ddfff8b2adadef699
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-06 17:10:15 -0300

    included fault_sec variable

--- SimpleInterferenceCase/define_FaultParams.m
+++ SimpleInterferenceCase/define_FaultParams.m
@@ -1,4 +1,6 @@
 
+% Number of fault's section:
+fault_sec = 1;
 
 % Fault incidence angle [rad or degress??]:
 app.Faultincidenceangle.Value = 90;
@@ -7,9 +9,7 @@ app.Faultincidenceangle.Value = 90;
 
 
 % Frequency [Hz]:
-
 app.Freq.Value = 60;
 
 % Cycles before fault:
-
 app.Cyclesbeforefault.Value = 0.5;

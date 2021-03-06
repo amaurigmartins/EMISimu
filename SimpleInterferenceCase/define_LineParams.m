41a184769152b2f95a31121e91ad0afc0d52d9f8
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-06 16:50:09 -0300

    added define_LineParams, including thevenin parameters

---
+++ SimpleInterferenceCase/define_LineParams.m
@@ -0,0 +1,30 @@
+
+% Number of circuits (1 or 2):
+app.NumCircuits.Value = 1;
+
+
+%% Thevenin params:
+
+% First circuit:
+app.R0S1T1.Value = 0.4853; %R0 [ohm/m]
+app.L0S1T1.Value = 6.0779; %L0 [mH/m]
+app.R1S1T1.Value = 0.151; %R+ [ohm/m]
+app.L1S1T1.Value = 1.8828; %L+ [mH/m]
+
+app.R0S1T2.Value = 0.4853; %R0 [ohm/m]
+app.L0S1T2.Value = 6.0779; %L0 [mH/m]
+app.R1S1T2.Value = 0.151; %R+ [ohm/m]
+app.L1S1T2.Value = 1.8828; %L+ [mH/m]
+
+
+% Second circuit:
+app.R0S2T1.Value = 0.4853; %R0 [ohm/m]
+app.L0S2T1.Value = 6.0779; %L0 [mH/m]
+app.R1S2T1.Value = 0.151; %R+ [ohm/m]
+app.L1S2T1.Value = 1.8828; %L+ [mH/m]
+
+app.R0S2T2.Value = 0.4853; %R0 [ohm/m]
+app.L0S2T2.Value = 6.0779; %L0 [mH/m]
+app.R1S2T2.Value = 0.151; %R+ [ohm/m]
+app.L1S2T2.Value = 1.8828; %L+ [mH/m]
+

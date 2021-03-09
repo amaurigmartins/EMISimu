cd33edb58d141ce10b0a9ab522a740d1d04844f5
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 18:39:01 -0300

    Included shield wires table convert in initialize_vars.m

--- SimpleInterferenceCase/initialize_vars.m
+++ SimpleInterferenceCase/initialize_vars.m
@@ -1,5 +1,3 @@
-
-
 % Ground impedance data table convert:
 app.UITableGroundImp.Data = num2cell(app.UITableGroundImp.Data);
 
@@ -11,3 +9,13 @@ Tow2Ground_Table(:,1) = Tow2Ground_transf(:,1,:);
 Tow2Ground_Table(:,2) = Tow2Ground_transf(:,2,:);
 
 app.UITableTow2Ground.Data = num2cell(Tow2Ground_Table);
+
+% Shield wires currents output specify sections table convert:
+
+SHCurr_transf = cell2mat(struct2cell(app.ShieldCurr));
+SHCurr_transf = struct2cell(SHCurr_transf);
+
+ShieldCurr_Table(:,1) = SHCurr_transf(:,1,:);
+ShieldCurr_Table(:,2) = SHCurr_transf(:,2,:);
+
+app.UITableSWCurr.Data = num2cell(ShieldCurr_Table);

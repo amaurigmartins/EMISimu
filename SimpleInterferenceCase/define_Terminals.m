3c0454d98139786a43afcd6c9cd0b606ee8b2851
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 15:25:02 -0300

    Added define_Terminals.m

---
+++ SimpleInterferenceCase/define_Terminals.m
@@ -0,0 +1,88 @@
+%% Terminal 1:
+
+% CIRCUIT 1 DATA:
+
+% VOLTAGE SOURCE:
+% RMS LL amplitude [V]:
+
+% Frequency [Hz]:
+
+% Phase angle [deg]:
+
+% Tstart [s]:
+
+% Tstop [s]:
+
+% Thevenin Eq.:
+
+% CIRCUIT 2 DATA (IF EXIST):
+
+% VOLTAGE SOURCE:
+% RMS LL amplitude [V]:
+
+% Frequency [Hz]:
+
+% Phase angle [deg]:
+
+% Tstart [s]:
+
+% Tstop [s]:
+
+% TERMINAL 1 SE IMPEDANCES:
+% Substation grounding grid:
+app.RGG1T1.Value = 1e-5; % R [ohms]
+app.LGG1T1.Value = 0; % L [mH]
+app.CGG1T1.Value = 0; % C [uF]
+
+% Substation neutral connection:
+app.RNC1T1.Value = 1e-5; % R [ohms]
+app.LNC1T1.Value = 0; % L [mH]
+app.CNC1T1.Value = 0; % C [uF]
+
+% Thevenin Eq.:
+
+
+
+
+%% Terminal 2:
+
+% CIRCUIT 1 DATA:
+
+% VOLTAGE SOURCE:
+% RMS LL amplitude [V]:
+
+% Frequency [Hz]:
+
+% Phase angle [deg]:
+
+% Tstart [s]:
+
+% Tstop [s]:
+
+% Thevenin Eq.:
+
+% CIRCUIT 2 DATA (IF EXIST):
+
+% VOLTAGE SOURCE:
+% RMS LL amplitude [V]:
+
+% Frequency [Hz]:
+
+% Phase angle [deg]:
+
+% Tstart [s]:
+
+% Tstop [s]:
+
+% TERMINAL 2 SE IMPEDANCES:
+% Substation grounding grid:
+app.RGG1T2.Value = 1e-5; % R [ohms]
+app.LGG1T2.Value = 0; % L [mH]
+app.CGG1T2.Value = 0; % C [uF]
+
+% Substation neutral connection:
+app.RNC1T2.Value = 1e-5; % R [ohms]
+app.LNC1T2.Value = 0; % L [mH]
+app.CNC1T2.Value = 0; % C [uF]
+
+% Thevenin Eq.:

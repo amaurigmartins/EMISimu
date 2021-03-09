8046917b17e6216cb0eeba15c9cc1a1a049ad3f0
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 15:53:29 -0300

    Added phase-to-ground and phase-to-phase resistances in define_FaultParams.m

--- SimpleInterferenceCase/define_FaultParams.m
+++ SimpleInterferenceCase/define_FaultParams.m
@@ -15,3 +15,9 @@ app.Freq.Value = 60;
 
 % Cycles before fault:
 app.Cyclesbeforefault.Value = 0.5;
+
+% Phase-to-phase resistance [Ohms]:
+app.Phasetophaseresistance.Value = 1e12;
+
+% Phase-to-ground resistance [Ohms]:
+app.Phasetogroundresistance.Value = 1e-5;

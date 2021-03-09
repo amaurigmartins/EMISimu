afdddd5242bc70e412b85c205e13518cda8e85fd
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 18:34:59 -0300

    Ended shield wires outputs

--- SimpleInterferenceCase/define_Outputs.m
+++ SimpleInterferenceCase/define_Outputs.m
@@ -23,3 +23,24 @@ app.OrsectionsbeforeandafterfaultTowShuntParam.Value = 0;
 
 % If "Yes" is selected, how many towers before and after fault:
 app.Tow2Ground.Value = 10;
+
+%% Shield wires outputs:
+
+% Shield wire return currents (Yes = 1 or No = 0):
+app.Shieldwirereturncurrents.Value = 1;
+
+% Specify sections to shield wire currents outputs:
+app.ShieldCurr(1).From.Value = 1;
+app.ShieldCurr(1).To.Value = 2;
+app.ShieldCurr(2).From.Value = 3;
+app.ShieldCurr(2).To.Value = 3;
+
+% Or sections before and after fault:
+
+% Yes -> 1 or No -> 0
+app.OrsectionsbeforeandafterfaultShWireCurr.Value = 0;
+
+% If "Yes" is selected, how many towers before and after fault:
+app.SWCurr.Value = 10;
+
+

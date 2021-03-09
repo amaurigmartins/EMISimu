df3de49b2b0dd79fac299f06c47f6e05d310f03e
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 15:23:34 -0300

    Changed variables in getSEImpedance.m

--- functions/getGroundImpedance.m
+++ functions/getGroundImpedance.m
@@ -1,6 +1,4 @@
 function [out] = getGroundImpedance(app,sec)
 
-app.UITableGroundImp.Data = num2cell(app.UITableGroundImp.Data);
-
 out=cell2mat(app.UITableGroundImp.Data(sec,:));
 end
--- functions/getSEImpedance.m
+++ functions/getSEImpedance.m
@@ -1,21 +1,21 @@
 function [out] = getSEImpedance(app,term)
 if term==1
-    out(1,1)=app.RGG1EditFieldT1.Value; %r ground
-    out(1,2)=app.LGG1EditFieldT1.Value; %l
-    out(1,3)=app.CGG1EditFieldT1.Value; %c
+    out(1,1)=app.RGG1T1.Value; %r ground
+    out(1,2)=app.LGG1T1.Value; %l
+    out(1,3)=app.CGG1T1.Value; %c
     
-    out(2,1)=app.RNC1EditFieldT1.Value; %r neutral
-    out(2,2)=app.LNC1EditFieldT1.Value; %l
-    out(2,3)=app.CNC1EditFieldT1.Value; %c
+    out(2,1)=app.RNC1T1.Value; %r neutral
+    out(2,2)=app.LNC1T1.Value; %l
+    out(2,3)=app.CNC1T1.Value; %c
     
 elseif term==2
-    out(1,1)=app.RGG1EditFieldT2.Value; %r ground
-    out(1,2)=app.LGG1EditFieldT2.Value; %l
-    out(1,3)=app.CGG1EditFieldT2.Value; %c
+    out(1,1)=app.RGG1T2.Value; %r ground
+    out(1,2)=app.LGG1T2.Value; %l
+    out(1,3)=app.CGG1T2.Value; %c
     
-    out(2,1)=app.RNC1EditFieldT2.Value; %r neutral
-    out(2,2)=app.LNC1EditFieldT2.Value; %l
-    out(2,3)=app.CNC1EditFieldT2.Value; %c
+    out(2,1)=app.RNC1T2.Value; %r neutral
+    out(2,2)=app.LNC1T2.Value; %l
+    out(2,3)=app.CNC1T2.Value; %c
     
 end
 end

1c047f2125859f845c8980bbc297f19db60f589e
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-09 14:15:10 -0300

    Ended Tower to ground outputs

--- SimpleInterferenceCase/define_Outputs.m
+++ SimpleInterferenceCase/define_Outputs.m
@@ -3,12 +3,12 @@
 %% Tower-to-ground outputs
 
 % Tower-to-ground voltages (Yes = 1 or No = 0):
-
+app.Towertogroundvoltages.Value = 1;
 
 % Tower-to-ground currents (Yes = 1 or No = 0):
+app.Towertogroundcurrents.Value = 1;
 
-
-% Specify sections:
+% Specify sections to Tower-to-ground outputs:
 Tow2Ground(1).from = 1;
 Tow2Ground(1).to = 3;
 Tow2Ground(2).from = 5;

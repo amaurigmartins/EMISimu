2c8bbe4f2ba9fda7cfe849da2234aaf25ca34ade
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-15 10:31:01 -0300

    Included shild wire, number of circuits and fault type in initialize_vars.m

--- SimpleInterferenceCase/define_Terminal1.m
+++ SimpleInterferenceCase/define_Terminal1.m
@@ -1,8 +1,7 @@
 %% Terminal 1:
 
-% CIRCUIT 1 DATA:
+%% CIRCUIT 1 DATA:
 
-% VOLTAGE SOURCE:
 % RMS LL amplitude [V]:
 app.RMSLLamplitudeC1T1.Value = 1000;
 
@@ -19,10 +18,14 @@ app.TstartsC1T1.Value = -1;
 app.TstopsC1T1.Value = 100;
 
 % Thevenin Eq.:
+app.R0S1T1.Value = 0.4853; %R0 [ohm/m]
+app.L0S1T1.Value = 6.0779; %L0 [mH/m]
+app.R1S1T1.Value = 0.151; %R+ [ohm/m]
+app.L1S1T1.Value = 1.8828; %L+ [mH/m]
 
-% CIRCUIT 2 DATA (IF EXIST):
 
-% VOLTAGE SOURCE:
+%% CIRCUIT 2 DATA (IF EXIST):
+
 % RMS LL amplitude [V]:
 app.RMSLLamplitudeC2T1.Value = 1000;
 
@@ -38,8 +41,14 @@ app.TstartsC2T1.Value = -1;
 % Tstop [s]:
 app.TstopsC2T1.Value = 100;
 
+% Thevenin Eq.:
+app.R0S2T1.Value = 0.4853; %R0 [ohm/m]
+app.L0S2T1.Value = 6.0779; %L0 [mH/m]
+app.R1S2T1.Value = 0.151; %R+ [ohm/m]
+app.L1S2T1.Value = 1.8828; %L+ [mH/m]
 
-% TERMINAL 1 SE IMPEDANCES:
+%% TERMINAL 1 SE IMPEDANCES:
+
 % Substation grounding grid:
 app.RGG1T1.Value = 1e-5; % R [ohms]
 app.LGG1T1.Value = 0; % L [mH]
@@ -50,60 +59,8 @@ app.RNC1T1.Value = 1e-5; % R [ohms]
 app.LNC1T1.Value = 0; % L [mH]
 app.CNC1T1.Value = 0; % C [uF]
 
-% Thevenin Eq.:
 
 
 
 
-%% Terminal 2:
 
-% CIRCUIT 1 DATA:
-
-% VOLTAGE SOURCE:
-% RMS LL amplitude [V]:
-app.RMSLLamplitudeC1T2.Value = 1000;
-
-% Frequency [Hz]:
-app.FrequencyC1T2.Value = 60;
-
-% Phase angle [deg]:
-app.PhaseangleC1T2.Value = 0;
-
-% Tstart [s]:
-app.TstartsC1T2.Value = -1;
-
-% Tstop [s]:
-app.TstopsC1T2.Value = 100;
-
-% Thevenin Eq.:
-
-% CIRCUIT 2 DATA (IF EXIST):
-
-% VOLTAGE SOURCE:
-% RMS LL amplitude [V]:
-app.RMSLLamplitudeC2T2.Value = 1000;
-
-% Frequency [Hz]:
-app.FrequencyC2T2.Value = 60;
-
-% Phase angle [deg]:
-app.PhaseangleC2T2.Value = 0;
-
-% Tstart [s]:
-app.TstartsC2T2.Value = -1;
-
-% Tstop [s]:
-app.TstopsC2T2.Value = 100;
-
-% TERMINAL 2 SE IMPEDANCES:
-% Substation grounding grid:
-app.RGG1T2.Value = 1e-5; % R [ohms]
-app.LGG1T2.Value = 0; % L [mH]
-app.CGG1T2.Value = 0; % C [uF]
-
-% Substation neutral connection:
-app.RNC1T2.Value = 1e-5; % R [ohms]
-app.LNC1T2.Value = 0; % L [mH]
-app.CNC1T2.Value = 0; % C [uF]
-
-% Thevenin Eq.:

f3663c1c2a611243e37e660dd12cf912b8b3dcfc
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-15 09:46:46 -0300

    Changed number of circuits and shield wires variables in define_LineParams.m

--- SimpleInterferenceCase/define_LineParams.m
+++ SimpleInterferenceCase/define_LineParams.m
@@ -1,7 +1,6 @@
 
 % Number of circuits (1 or 2):
-app.NumCircuits.Value = 1;
-
+app.NumberofCircuits.Value = 1;
 
 % Number of sections:
 app.Numberofsections.Value = 50;
@@ -15,14 +14,9 @@ app.UITableGroundImp.Data = load('ground_impedance_data.csv');
 
 %% Shield wires
 
-% None shield wires:
-app.NoneSW.Value = 0;
+% Number of shield wires (0, 1 or 2):
+app.NumberofShieldWires.Value = 1;
 
-% Single shield wires:
-app.SingleSW.Value = 1;
-
-% Doble shield wires:
-app.DoubleSW.value = 0;
 
 
 

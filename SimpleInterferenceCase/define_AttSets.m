f29389658320adc37231529eb242ef57bfc123e1
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-16 16:30:25 -0300

    Included file define_AttSets.m

---
+++ SimpleInterferenceCase/define_AttSets.m
@@ -0,0 +1,12 @@
+%% AttSet Default Number:
+
+app.Defaultattributeset.Value = 1;
+
+%% Specify AttSets sections:
+
+app.AttSetSec(1).From.Value = 1;
+app.AttSetSec(1).To.Value = 2;
+app.AttSetSec(1).AttSetNumber.Value = 1;
+app.AttSetSec(2).From.Value = 3;
+app.AttSetSec(2).To.Value = 3;
+app.AttSetSec(2).AttSetNumber.Value = 2;

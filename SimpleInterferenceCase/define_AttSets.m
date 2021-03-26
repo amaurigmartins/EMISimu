8a4abd60f902635fbcf791aacfc20c71e7dd2e92
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-03-26 14:52:53 -0300

    Included second AttSet in define_AttSets.m

--- SimpleInterferenceCase/define_AttSets.m
+++ SimpleInterferenceCase/define_AttSets.m
@@ -10,3 +10,51 @@ app.AttSetSec(1).AttSetNumber.Value = 1;
 app.AttSetSec(2).From.Value = 3;
 app.AttSetSec(2).To.Value = 3;
 app.AttSetSec(2).AttSetNumber.Value = 2;
+
+%% AttSet 1:
+
+app.AttSet(1).Phase(1).condID.Value = 'Peacock';
+app.AttSet(1).Phase(1).numberofCond.Value = 2;
+app.AttSet(1).Phase(1).spaceBetweenCond.Value = 0.4;
+app.AttSet(1).Phase(1).coordX.Value = -1;
+app.AttSet(1).Phase(1).coordY.Value = 20;
+app.AttSet(1).Phase(2).condID.Value = 'Peacock';
+app.AttSet(1).Phase(2).numberofCond.Value = 2;
+app.AttSet(1).Phase(2).spaceBetweenCond.Value = 0.4;
+app.AttSet(1).Phase(2).coordX.Value = 0;
+app.AttSet(1).Phase(2).coordY.Value = 20;
+app.AttSet(1).Phase(3).condID.Value = 'Peacock';
+app.AttSet(1).Phase(3).numberofCond.Value = 2;
+app.AttSet(1).Phase(3).spaceBetweenCond.Value = 0.4;
+app.AttSet(1).Phase(3).coordX.Value = 1;
+app.AttSet(1).Phase(3).coordY.Value = 20;
+app.AttSet(1).ShieldWire(1).condID.Value = 'Steel 3/8 EHS';
+app.AttSet(1).ShieldWire(1).coordX.Value = 0;
+app.AttSet(1).ShieldWire(1).coordY.Value = 22;
+app.AttSet(1).Target(1).condID.Value = 'Pipe 16" STD';
+app.AttSet(1).Target(1).coordX.Value = 0;
+app.AttSet(1).Target(1).coordY.Value = -1;
+
+%% AttSet 2:
+
+app.AttSet(2).Phase(1).condID.Value = 'Grosbeak';
+app.AttSet(2).Phase(1).numberofCond.Value = 2;
+app.AttSet(2).Phase(1).spaceBetweenCond.Value = 0.4;
+app.AttSet(2).Phase(1).coordX.Value = -1;
+app.AttSet(2).Phase(1).coordY.Value = 20;
+app.AttSet(2).Phase(2).condID.Value = 'Grosbeak';
+app.AttSet(2).Phase(2).numberofCond.Value = 2;
+app.AttSet(2).Phase(2).spaceBetweenCond.Value = 0.4;
+app.AttSet(2).Phase(2).coordX.Value = 0;
+app.AttSet(2).Phase(2).coordY.Value = 20;
+app.AttSet(2).Phase(3).condID.Value = 'Grosbeak';
+app.AttSet(2).Phase(3).numberofCond.Value = 2;
+app.AttSet(2).Phase(3).spaceBetweenCond.Value = 0.4;
+app.AttSet(2).Phase(3).coordX.Value = 1;
+app.AttSet(2).Phase(3).coordY.Value = 20;
+app.AttSet(2).ShieldWire(1).condID.Value = 'Steel 3/8 EHS';
+app.AttSet(2).ShieldWire(1).coordX.Value = 0;
+app.AttSet(2).ShieldWire(1).coordY.Value = 22;
+app.AttSet(2).Target(1).condID.Value = 'Pipe 18" STD';
+app.AttSet(2).Target(1).coordX.Value = 0;
+app.AttSet(2).Target(1).coordY.Value = -1;

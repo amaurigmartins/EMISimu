830d1193bb90fe19a28ee33a7cb4f6453dd42d1c
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-06-01 09:55:19 -0300

    Changed variables in define_FaultParams

--- SimpleInterferenceCase/define_FaultParams.m
+++ SimpleInterferenceCase/define_FaultParams.m
@@ -1,9 +1,11 @@
 
 % Faulted circuit: (Circuit 1 or Circuit 2)
-app.Faultedcircuit.Value = 1;
+app.Faultedcircuit.SelectedObject.Text = 'Circuit 1';
 
 % Number of fault's section:
-app.fault_sec = 1;
+app.FaultedSection(1).From.Value = 1;
+app.FaultedSection(1).To.Value = 3;
+
 
 % Fault incidence angle [degress]:
 app.Faultincidenceangle.Value = 90;
@@ -11,7 +13,7 @@ app.Faultincidenceangle.Value = 90;
 % Fault Type:
 % Types: 1 - AG, 2 - BG, 3 - CG, 4 - AB, 5 - BC, 
 % 6 - CA, 7 - ABG, 8 - BCG, 9 - CAG, 10 - ABC, 11 - ABCG.
-app.FaulttypeFTYPEButtonGroup.Value = 1;
+app.FaulttypeFTYPEButtonGroup.SelectedObject.Text = '1 - AG';
 
 % Frequency [Hz]:
 app.Freq.Value = 60;
@@ -20,7 +22,9 @@ app.Freq.Value = 60;
 app.Cyclesbeforefault.Value = 0.5;
 
 % Phase-to-phase resistance [Ohms]:
-app.Phasetophaseresistance.Value = 1e12;
+app.Phasetophaseresistance.Value = 1e5;
 
 % Phase-to-ground resistance [Ohms]:
 app.Phasetogroundresistance.Value = 1e-5;
+
+

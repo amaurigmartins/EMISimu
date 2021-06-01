edff5a73a332247ed3510c04bc6beda657ddfc9c
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-06-01 10:00:26 -0300

    Changed variables in initialize_CouplingRegions

--- functions/initialize_CouplingRegions.m
+++ functions/initialize_CouplingRegions.m
@@ -6,12 +6,14 @@ minTargetLength = getMinTargetLength(app);
 
 [CoupRegionsTable, matrixLCC] = calcCouplingRegionsParams(srcCoord,tgtCoord,minTargetLength);
 
+app.UITableSL.Data = CoupRegionsTable(:,7);
+
 app.CoupRegTable.EquivalentDistance.Data = matrixLCC(:,1);
 app.CoupRegTable.EquivalentLength.Data = matrixLCC(:,2);
 app.CoupRegTable.AccTargetDistance.Data = matrixLCC(:,3);
 app.CoupRegTable.SourceGroundingIndex.Data = matrixLCC(:,4);
-app.CoupRegTable.TargetSpanLength.Data = CoupRegionsTable(:,7);
-app.CoupRegTable.SourceSpanLength.Data = CoupRegionsTable(:,8);
+app.CoupRegTable.TargetSpanLength.Data = CoupRegionsTable(:,8);
+app.CoupRegTable.SourceSpanLength.Data = CoupRegionsTable(:,7);
 app.CoupRegTable.SubTargetCoordX.Data = CoupRegionsTable(:,3);
 app.CoupRegTable.SubTargetCoordY.Data = CoupRegionsTable(:,4);
 app.CoupRegTable.SubSourceCoordX.Data = CoupRegionsTable(:,1);

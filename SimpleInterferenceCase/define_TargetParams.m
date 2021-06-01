140a325ffc28e9be066664cae61f829fc5c7b66d
CaioMissiaggia <caiomissiaggia96@gmail.com>
2021-06-01 09:58:48 -0300

    Changed variables in define_TargetParams

--- SimpleInterferenceCase/define_TargetParams.m
+++ SimpleInterferenceCase/define_TargetParams.m
@@ -5,13 +5,3 @@ app.NumTargets.Value = 5;
 % Minimum target length subdivision in [m]:
 
 app.MinTargetLength.Value = 50;
-
-
-% Coating resistivity in [ohm.m]
-app.CoatingResistivity.Value = 1e8;
-
-% Coating thickness in [m]
-app.CoatingThickness.Value = 3e-3;
-
-% Coating Relative permeability
-app.CoatingRelativePermeability.Value = 1;

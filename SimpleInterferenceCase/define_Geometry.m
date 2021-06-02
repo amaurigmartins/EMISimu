0db99f706f3fd42520eb100b450b6ace57f81ead
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-02 16:56:20 -0300

    Changed path

--- SimpleInterferenceCase/define_Geometry.m
+++ SimpleInterferenceCase/define_Geometry.m
@@ -1,4 +1,4 @@
 %% Coordinates files [X Y] in meters:
 
-app.TargetCoordFile.Text ='D:\Mestrado\EMISimu\SimpleInterferenceCase\OSSP_RECAP.csv';
-app.SourceCoordFile.Text ='D:\Mestrado\EMISimu\SimpleInterferenceCase\LT_RECAP.csv';
+app.TargetCoordFile.Text ='D:\Caio\EMISimu\SimpleInterferenceCase\OSSP_RECAP.csv';
+app.SourceCoordFile.Text ='D:\Caio\EMISimu\SimpleInterferenceCase\LT_RECAP.csv';

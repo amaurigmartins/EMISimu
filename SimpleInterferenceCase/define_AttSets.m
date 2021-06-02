61bb2fb4506d6d90bddb6769bd23b66a15d9ad87
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-02 17:00:06 -0300

    Changed variables

--- SimpleInterferenceCase/define_AttSets.m
+++ SimpleInterferenceCase/define_AttSets.m
@@ -5,10 +5,10 @@ app.Defaultattributeset.Value = 1;
 %% Specify AttSets sections:
 
 app.AttSetSec(1).From.Value = 1;
-app.AttSetSec(1).To.Value = 2;
+app.AttSetSec(1).To.Value = 5;
 app.AttSetSec(1).AttSetNumber.Value = 1;
-app.AttSetSec(2).From.Value = 3;
-app.AttSetSec(2).To.Value = 3;
+app.AttSetSec(2).From.Value = 6;
+app.AttSetSec(2).To.Value = 13;
 app.AttSetSec(2).AttSetNumber.Value = 2;
 
 %% AttSet 1:
@@ -59,7 +59,7 @@ app.AttSet(2).Phase(3).coordY.Value = 20;
 app.AttSet(2).ShieldWire(1).condID.Value = 'Steel 3/8 EHS';
 app.AttSet(2).ShieldWire(1).coordX.Value = 0;
 app.AttSet(2).ShieldWire(1).coordY.Value = 22;
-app.AttSet(2).Target(1).condID.Value = 'Pipe 18" STD';
+app.AttSet(2).Target(1).condID.Value = 'Pipe 16" STD';
 app.AttSet(2).Target(1).coordX.Value = 0;
 app.AttSet(2).Target(1).coordY.Value = -1;
 app.AttSet(2).Target(1).CoatingResistivity.Value = 1e8;

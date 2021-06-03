84e67cb1033f366bc98e9d8fc70fdea76c7c0910
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-02 21:38:58 -0300

    Changed modprin function

--- functions/writeLCCConductorCard.m
+++ functions/writeLCCConductorCard.m
@@ -4,11 +4,11 @@ out=blanks(80);
 %12345678901234567890123456789012345678901234567890123456789012345678901234567890
 %C >SKIN>RESIS  >I>REACT  >DIAM   >HORIZ  >VTOWER >VMID   >SEPAR  >ALPHA>      NB
 out(1:3)=sprintf('%3d',con(1));
-out(4:8)=modprin(app,5,con(2));
-out(9:16)=modprin(app,8,con(3));
+out(4:8)=modprin(5,con(2));
+out(9:16)=modprin(8,con(3));
 out(17:18)=sprintf('%2d',con(4));
-out(27:34)=modprin(app,8,con(5));
-out(35:42)=modprin(app,8,con(6));
-out(43:50)=modprin(app,8,con(7));
-out(51:58)=modprin(app,8,con(8));
+out(27:34)=modprin(8,con(5));
+out(35:42)=modprin(8,con(6));
+out(43:50)=modprin(8,con(7));
+out(51:58)=modprin(8,con(8));
 end

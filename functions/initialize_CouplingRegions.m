2015028eaac1662481cac233a513667087e19333
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-06 18:28:42 -0300

    Changed variables

--- functions/initialize_CouplingRegions.m
+++ functions/initialize_CouplingRegions.m
@@ -4,22 +4,22 @@ srcCoord = getSourceCoords(app);
 tgtCoord = getTargetCoords(app);
 minTargetLength = getMinTargetLength(app);
 
-[CoupRegionsTable, matrixLCC] = calcCouplingRegionsParams(srcCoord,tgtCoord,minTargetLength);
+[GeometricData, LCCData] = calcCouplingRegionsParams(srcCoord,tgtCoord,minTargetLength);
 
-app.UITableSL.Data = num2cell(matrixLCC(:,2));
+app.UITableSL.Data = num2cell(LCCData(:,2));
 
-app.CoupRegTable.EquivalentDistance.Data = matrixLCC(:,1);
-app.CoupRegTable.EquivalentLength.Data = matrixLCC(:,2);
-app.CoupRegTable.AccTargetDistance.Data = matrixLCC(:,3);
-app.CoupRegTable.SourceGroundingIndex.Data = matrixLCC(:,4);
-app.CoupRegTable.TargetSpanLength.Data = CoupRegionsTable(:,8);
-app.CoupRegTable.SourceSpanLength.Data = CoupRegionsTable(:,7);
-app.CoupRegTable.SubTargetCoordX.Data = CoupRegionsTable(:,3);
-app.CoupRegTable.SubTargetCoordY.Data = CoupRegionsTable(:,4);
-app.CoupRegTable.SubSourceCoordX.Data = CoupRegionsTable(:,1);
-app.CoupRegTable.SubSourceCoordY.Data = CoupRegionsTable(:,2);
+app.CoupRegTable.EquivalentDistance.Data = LCCData(:,1);
+app.CoupRegTable.EquivalentLength.Data = LCCData(:,2);
+app.CoupRegTable.AccTargetDistance.Data = LCCData(:,3);
+app.CoupRegTable.SourceGroundingIndex.Data = LCCData(:,4);
+app.CoupRegTable.TargetSpanLength.Data = GeometricData(:,8);
+app.CoupRegTable.SourceSpanLength.Data = GeometricData(:,7);
+app.CoupRegTable.SubTargetCoordX.Data = GeometricData(:,3);
+app.CoupRegTable.SubTargetCoordY.Data = GeometricData(:,4);
+app.CoupRegTable.SubSourceCoordX.Data = GeometricData(:,1);
+app.CoupRegTable.SubSourceCoordY.Data = GeometricData(:,2);
 
-app.Numberofsections.Value = size(matrixLCC,1);
+app.Numberofsections.Value = size(LCCData,1);
 
 
 

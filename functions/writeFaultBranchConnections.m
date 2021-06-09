4582dbdaf0052817593fb7d202a91fda30fd827e
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-09 19:38:22 -0300

    Included permanent regime in fault block

--- functions/writeFaultBranchConnections.m
+++ functions/writeFaultBranchConnections.m
@@ -1,5 +1,6 @@
 function out = writeFaultBranchConnections(app,fault_sec)
 
+fault_type = getFaultType(app);
 measure_status=valIfTrue(app,app.Faultbranchphasecurrents.Value,1,0);
 circ_fault=valIfTrue(app, strcmp(app.Faultedcircuit.SelectedObject.Text,'Circuit 1'),1,2);
 seq='ABC';
@@ -14,20 +15,35 @@ out1=strcat(out1, '13RFA   RG_A                                                 
 out2=blanks(80);
 out2(3:8)=sprintf('%d%04d%s',circ_fault,fault_sec,seq(1));
 out2(9:13)='F_INA';
-out2(55:63)='MEASURING';
-out2(80)=sprintf('%d',measure_status);
+if fault_type ~= 0 % permanent regime (None fault)
+    out2(55:63)='MEASURING';
+    out2(80)=sprintf('%d',measure_status);
+else
+    out2(15:24)='      1.E3';
+    out2(80)='0';
+end
 
 out3=blanks(80);
 out3(3:8)=sprintf('%d%04d%s',circ_fault,fault_sec,seq(2));
 out3(9:13)='F_INB';
-out3(55:63)='MEASURING';
-out3(80)=sprintf('%d',measure_status);
+if fault_type ~= 0 % permanent regime (None fault)
+    out3(55:63)='MEASURING';
+    out3(80)=sprintf('%d',measure_status);
+else
+    out3(15:24)='      1.E3';
+    out3(80)='0';
+end
 
 out4=blanks(80);
 out4(3:8)=sprintf('%d%04d%s',circ_fault,fault_sec,seq(3));
 out4(9:13)='F_INC';
-out4(55:63)='MEASURING';
-out4(80)=sprintf('%d',measure_status);
+if fault_type ~= 0 % permanent regime (None fault)
+    out4(55:63)='MEASURING';
+    out4(80)=sprintf('%d',measure_status);
+else
+    out4(15:24)='      1.E3';
+    out4(80)='0';
+end
 
 out5=blanks(80);
 out5(3:8)='FGOUTA';

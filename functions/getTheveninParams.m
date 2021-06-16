4d91e55191dcc7571e1d3f0242b3edfc0193a715
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-16 15:26:58 -0300

    Changed variables

--- functions/getTheveninParams.m
+++ functions/getTheveninParams.m
@@ -2,15 +2,15 @@ function [term1, term2] = getTheveninParams(app)
 ncirc=valIfTrue(app,app.DoublecircuitButton.Value==1,2,1);
 
 
-term1(1,1)=app.R0S1EditFieldT1.Value; %R0
-term1(1,2)=app.L0S1EditFieldT1.Value; %L0
-term1(1,3)=app.R1S1EditFieldT1.Value; %R+
-term1(1,4)=app.L1S1EditFieldT1.Value; %L+
+term1(1,1)=app.R0S1T1.Value; %R0
+term1(1,2)=app.L0S1T1.Value; %L0
+term1(1,3)=app.R1S1T1.Value; %R+
+term1(1,4)=app.L1S1T1.Value; %L+
 
-term2(1,1)=app.R0S1EditFieldT2.Value; %R0
-term2(1,2)=app.L0S1EditFieldT2.Value; %L0
-term2(1,3)=app.R1S1EditFieldT2.Value; %R+
-term2(1,4)=app.L1S1EditFieldT2.Value; %L+
+term2(1,1)=app.R0S1T2.Value; %R0
+term2(1,2)=app.L0S1T2.Value; %L0
+term2(1,3)=app.R1S1T2.Value; %R+
+term2(1,4)=app.L1S1T2.Value; %L+
 
 if ncirc==2
     term1(2,1)=app.R0S2T1.Value; %R0

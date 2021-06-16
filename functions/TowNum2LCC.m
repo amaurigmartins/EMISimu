e458478d8b7e2f88523c7b03a1d58224e92a0c6e
amaurigmartins <amaurigmartins@gmail.com>
2021-06-16 11:43:12 -0300

    Added comments

--- functions/TowNum2LCC.m
+++ functions/TowNum2LCC.m
@@ -1,11 +1,11 @@
 function out = TowNum2LCC(app,towers)
 
-% towers = vetor com os numeros das torres
+% towers = vetor com os numeros das torres [1 2 3 4 4 666 1234]
 
 [out,~] = find(app.CoupRegTable.SourceGroundingIndex.Data == 1);
 
 out = [0;out];
 
-% out é algum processamento desse vvetor
+% out é algum processamento desse vvetor que sai o numero do LCC
 
 end

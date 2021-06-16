0c756b1718b75143d661cc84f4c00a1e6adbc16a
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-16 15:22:12 -0300

    Added "{}"

--- functions/getProjectDesc.m
+++ functions/getProjectDesc.m
@@ -1,5 +1,5 @@
 function out = getProjectDesc(app)
-out=app.Projectdescription.Text;
+out={app.Projectdescription.Text};
 out=cellfun(@(x) (['C ' x(:,:) '\n']),out,'UniformOutput',false);
 out=join(out,"");
 out=char(out);

fb25a4dc53de1ccc5208ba25aba7a94d4829f423
Bia Missiaggia <Bia Missiaggia@DESKTOP-AJBAI8E>
2021-06-18 11:52:37 -0300

    Added LCC2TowNum function

---
+++ functions/LCC2TowNum.m
@@ -0,0 +1,19 @@
+function out = LCC2TowNum(app,LCCnumber)
+
+nsec = getNumSections(app);
+
+ntw = getNumTowers(app);
+
+out = ones(nsec,1);
+
+Tow2LCC = TowNum2LCC(app,1:ntw);
+
+Tow2LCC(1) = 1;
+
+for i=2:ntw
+   out(Tow2LCC(i-1)+1:Tow2LCC(i)) = i;
+end
+
+out = out(LCCnumber);
+
+end

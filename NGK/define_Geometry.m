4b0b858a4ebd37b81b6c30055605812f6008675f
caiom <caiom@DESKTOP-3G7TE3N>
2021-07-15 14:42:03 -0300

    NGK model

---
+++ NGK/define_Geometry.m
@@ -0,0 +1,4 @@
+%% Coordinates files [X Y] in meters:
+
+app.TargetCoordFile.Text ='D:\Mestrado\EMISimu\SimpleInterferenceCase\OSSP_RECAP.csv';
+app.SourceCoordFile.Text ='D:\Mestrado\EMISimu\SimpleInterferenceCase\LT_RECAP.csv';
---
+++ NGK/define_TargetParams.m
@@ -0,0 +1,18 @@
+%% Number of targets:
+
+app.NumTargets.Value = 1;
+
+% Minimum target length subdivision in [m]:
+
+app.MinTargetLength.Value = 25;
+
+%% TERMINAL 1 TARGET IMPEDANCES:
+
+% Terminal grounding impedance:
+app.Target(1).GroundImpedanceT1.Value = 10; % R [ohms]
+
+
+%% TERMINAL 2 TARGET IMPEDANCES:
+
+% Terminal grounding impedance:
+app.Target(1).GroundImpedanceT2.Value = 10; % R [ohms]

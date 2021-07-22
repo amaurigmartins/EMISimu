eb02115756a5d4f7d4df77b14150a9c527483e7c
caiom <caiom@DESKTOP-3G7TE3N>
2021-07-22 08:27:23 -0300

    Included Display Name in target params

--- SES/define_TargetParams.m
+++ SES/define_TargetParams.m
@@ -16,3 +16,8 @@ app.Target(1).GroundImpedanceT1.Value = 10; % R [ohms]
 
 % Terminal grounding impedance:
 app.Target(1).GroundImpedanceT2.Value = 10; % R [ohms]
+
+
+%% TARGET DISPLAY NAME
+
+app.Target(1).DisplayName.Text = 'Horizontal SES 14"';

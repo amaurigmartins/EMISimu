2eea050d361750f087c72e1bcd85b643f878f85c
Amauri Martins <amaurigmartins@gmail.com>
2022-06-26 15:49:38 -0300

    Delete HorizontalDuploCruzamento directory

--- HorizontalDuploCruzamento/define_ATPSettings.m
+++
@@ -1,6 +0,0 @@
-
-% dT 
-app.deltaT.Value = 1e-6;
-
-% Tmax simulation [s]
-app.Tmax.Value = 0.1;
--- HorizontalDuploCruzamento/define_Geometry.m
+++
@@ -1,4 +0,0 @@
-%% Coordinates files [X Y] in meters:
-
-app.TargetCoordFile.Text ='D:\Mestrado\EMISimu\HorizontalDuploCruzamento\DT_2cruz.csv';
-app.SourceCoordFile.Text ='D:\Mestrado\EMISimu\HorizontalDuploCruzamento\LT_2cruz.csv';

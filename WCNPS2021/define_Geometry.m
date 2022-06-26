48fe549a857b5f0642558a154b7be6acd159d9e0
Amauri Martins <amaurigmartins@gmail.com>
2022-06-26 15:49:00 -0300

    Delete WCNPS2021 directory

--- WCNPS2021/define_Geometry.m
+++
@@ -1,4 +0,0 @@
-%% Coordinates files [X Y] in meters:
-
-app.TargetCoordFile.Text ='D:\Mestrado\EMISimu\WCNPS2021\DT_WCNPS.csv';
-app.SourceCoordFile.Text ='D:\Mestrado\EMISimu\WCNPS2021\LT_WCNPS.csv';

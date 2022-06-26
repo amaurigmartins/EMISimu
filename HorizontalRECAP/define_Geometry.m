d74aec2bb8935a88d79c5b5187fbd722bb783cc9
Amauri Martins <amaurigmartins@gmail.com>
2022-06-26 15:49:53 -0300

    Delete HorizontalRECAP directory

--- HorizontalRECAP/define_Geometry.m
+++
@@ -1,4 +0,0 @@
-%% Coordinates files [X Y] in meters:
-
-app.TargetCoordFile.Text ='D:\Mestrado\EMISimu\HorizontalRECAP\OSSP_RECAP.csv';
-app.SourceCoordFile.Text ='D:\Mestrado\EMISimu\HorizontalRECAP\LT_RECAP.csv';
--- HorizontalRECAP/define_Soil.m
+++
@@ -1,3 +0,0 @@
-
-% Soil Resistivity per section .csv file [ohm.m]:
-app.SoilResistivityFile.Text = 'D:\Mestrado\EMISimu\HorizontalRECAP\soil_resistivity_data.csv';
